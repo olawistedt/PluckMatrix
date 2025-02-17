dee7e208a71785de2cb20b02333c7f956aa43a64
olawistedt <43708280+olawistedt@users.noreply.github.com>
2025-02-17 14:12:59 +0000

    Deploying to gh-pages from @ olawistedt/PluckMatrix@2f95cd01d785af9f180fff94991afa03c3250c66 🚀

--- imgs.js
+++ imgs.js
@@ -345,7 +345,7 @@ Module['FS_createPath']("/resources", "img", true, true);
     }
 
     }
-    loadPackage({"files": [{"filename": "/resources/img/Background.png", "start": 0, "end": 37138}, {"filename": "/resources/img/Background.xcf", "start": 37138, "end": 96569}, {"filename": "/resources/img/btnLed.png", "start": 96569, "end": 101248}, {"filename": "/resources/img/btnNote.png", "start": 101248, "end": 103320}], "remote_package_size": 103320, "package_uuid": "sha256-29798217ded085d99833428ba96e7d578fda34f4d50608dcbddba16ef3a0e31b"});
+    loadPackage({"files": [{"filename": "/resources/img/btnLed.png", "start": 0, "end": 4679}, {"filename": "/resources/img/btnProp-Brons.png", "start": 4679, "end": 6751}], "remote_package_size": 6751, "package_uuid": "sha256-7cb6cb3193a9d36c12435e165fc6488b7f5ae1afc1c58fa32dbef7d703379f71"});
 
   })();
 
